dca2fad465e5bfb623fe7d3def5689d447f364d0
Amornwut Lertsiriumnouyphon <petchy125@hotmail.com>
2023-09-25 17:05:19 +0700

    move all files from nerdflix 1

---
+++ apps/films/app/(protected)/films/page.tsx
@@ -0,0 +1,42 @@
+import Hero from '@nerdflix-nx-cypress/shared/components/hero';
+import { getSearchedResult, getShow } from '@/lib/fetcher';
+import Collections from '../../components/collections';
+import { getRandomShow } from '@/lib/util';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Nerdflix | Films',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Record<PropertyKey, string>;
+}) {
+  const allShows = await getShow('movie');
+  const searchedResults = await getSearchedResult(searchParams?.search ?? '');
+
+  const randomShow = getRandomShow(allShows.netflix);
+
+  const collections = [
+    { title: 'Trending', shows: allShows.trending },
+    { title: 'Top Rated', shows: allShows.topRated },
+    { title: 'Action', shows: allShows.action },
+    { title: 'Comedy', shows: allShows.comedy },
+    { title: 'Horror', shows: allShows.horror },
+    { title: 'Romance', shows: allShows.romance },
+    { title: 'Documentary', shows: allShows.docs },
+  ];
+
+  return (
+    <section>
+      <div className="pt-10">
+        {searchedResults.length < 1 && <Hero type="show" show={randomShow} />}
+        <Collections
+          collections={collections}
+          searchedResults={searchedResults}
+        />
+      </div>
+    </section>
+  );
+}
---
+++ apps/series/app/(protected)/series/page.tsx
@@ -0,0 +1,39 @@
+import Hero from '@/app/components/hero';
+import { getSearchedResult, getShow } from '@/lib/fetcher';
+import Collections from '../../components/collections';
+import { getRandomShow } from '@/lib/util';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Nerdflix | Series',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Record<PropertyKey, string>;
+}) {
+  const allShows = await getShow('tv');
+  const searchedResults = await getSearchedResult(searchParams?.search ?? '');
+  const randomShow = getRandomShow(allShows.netflix);
+
+  const collections = [
+    { title: 'Trending', shows: allShows.trending },
+    { title: 'Top Rated', shows: allShows.topRated },
+    { title: 'Comedy', shows: allShows.comedy },
+    { title: 'Romance', shows: allShows.romance },
+    { title: 'Documentary', shows: allShows.docs },
+  ];
+
+  return (
+    <section>
+      <div className="pt-10">
+        {searchedResults.length < 1 && <Hero type="show" show={randomShow} />}
+        <Collections
+          collections={collections}
+          searchedResults={searchedResults}
+        />
+      </div>
+    </section>
+  );
+}
