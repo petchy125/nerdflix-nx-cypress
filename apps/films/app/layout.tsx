de4cb6534f8ec4559ef194bbdfd5c5018227f2f4
Amornwut Lertsiriumnouyphon <petchy125@hotmail.com>
2023-09-26 17:22:09 +0700

    add config to libs

--- apps/films/app/layout.tsx
+++ apps/films/app/layout.tsx
@@ -1,40 +1,75 @@
-import type { Metadata } from 'next';
+import { PropsWithChildren } from 'react';
+
+import { Nav } from '@nerdflix-nx-cypress/shared/container/nav';
+import Footer from '@nerdflix-nx-cypress/shared/container/footer';
 import { getServerSession } from 'next-auth';
 
-import SessionProvider from '@nerdflix-nx-cypress/shared/providers/session-provider';
-import { authOptions } from './api/auth/[...nextauth]/route';
-import '@nerdflix-nx-cypress/shared/assets/styles/global.css';
-import Script from 'next/script';
+export default async function ProtectedLayout({ children }: PropsWithChildren) {
+  const session = await getServerSession();
 
-export const metadata: Metadata = {
-  title: 'Nerdflix',
-  description: 'An application built by nerds',
-};
-
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const session = await getServerSession(authOptions);
   return (
-    <html lang="en">
-      <body>
-        <SessionProvider session={session}>{children}</SessionProvider>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-        />
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', '${process.env.GOOGLE_ANALYTICS}');
-        `}
-        </Script>
-      </body>
-    </html>
+    <div className="flex min-h-screen flex-col">
+      <Nav.default>
+        <Nav.Frame>
+          <Nav.Group>
+            <Nav.Logo />
+            <Nav.TextLink href="/series">Series</Nav.TextLink>
+            <Nav.TextLink href="/films">Films</Nav.TextLink>
+          </Nav.Group>
+          <Nav.Group>
+            <Nav.Search />
+            <Nav.Profile>
+              <Nav.Picture />
+              <Nav.Dropdown>
+                <Nav.Group>
+                  <Nav.Picture className="mr-3" />
+                  <Nav.TextLink href="#">{session?.user?.email}</Nav.TextLink>
+                </Nav.Group>
+                <Nav.Group>
+                  <Nav.SignOutButton>Sign Out</Nav.SignOutButton>
+                </Nav.Group>
+              </Nav.Dropdown>
+            </Nav.Profile>
+          </Nav.Group>
+        </Nav.Frame>
+      </Nav.default>
+      <main className="flex-1">{children}</main>
+      <Footer>
+        <Footer.Title>Questions? Contact us.</Footer.Title>
+        <Footer.Break />
+        <Footer.Row>
+          <Footer.Column>
+            <Footer.Link href="#">FAQ</Footer.Link>
+            <Footer.Link href="#">Investor Relations</Footer.Link>
+            <Footer.Link href="#">Ways to Watch</Footer.Link>
+            <Footer.Link href="#">Corporate Information</Footer.Link>
+            <Footer.Link href="#">Netflix Originals</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Help Centre</Footer.Link>
+            <Footer.Link href="#">Jobs</Footer.Link>
+            <Footer.Link href="#">Terms of Use</Footer.Link>
+            <Footer.Link href="#">Contact Us</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Account</Footer.Link>
+            <Footer.Link href="#">Redeem gift cards</Footer.Link>
+            <Footer.Link href="#">Privacy</Footer.Link>
+            <Footer.Link href="#">Speed Test</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Media Centre</Footer.Link>
+            <Footer.Link href="#">Buy gift cards</Footer.Link>
+            <Footer.Link href="#">Cookie Preferences</Footer.Link>
+            <Footer.Link href="#">Legal Notices</Footer.Link>
+          </Footer.Column>
+        </Footer.Row>
+        <Footer.Break />
+        <Footer.Text>Netflix United Kingdom</Footer.Text>
+      </Footer>
+    </div>
   );
 }
